ae0af8c30bc5e495f72ae475984929c99ef08d6c
balderi <balderi@tsponline.dk>
2018-12-25 03:32:24 +0100

    UIElement event remajiggering

--- TerminalGame/UI/Elements/UIElement.cs
+++ TerminalGame/UI/Elements/UIElement.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,9 @@ namespace TerminalGame.UI.Elements
         #region fields
         protected SpriteBatch _spriteBatch;
         protected float _opacity, _fadeTarget;
-        protected bool _fadingUp, _fadingDown;
+        protected bool _fadingUp, _fadingDown, _isHovering, _newMouseHoverState, _previousMouseHoverState;
+        protected MouseState _previousMouseState, _currentMouseState;
+        protected readonly MouseEventArgs _hover, _click, _enter, _leave;
         #endregion
 
         #region properties
@@ -21,10 +24,15 @@ namespace TerminalGame.UI.Elements
         #endregion
 
         #region events
-        public event EventHandler MouseHover;
-        public event EventHandler MouseEnter;
-        public event EventHandler MouseLeave;
-        public event EventHandler Click;
+        public delegate void MouseHoverEventHandler(MouseEventArgs e);
+        public delegate void MouseClickEventHandler(MouseEventArgs e);
+        public delegate void MouseEnterEventHandler(MouseEventArgs e);
+        public delegate void MouseLeaveEventHandler(MouseEventArgs e);
+
+        public event MouseHoverEventHandler MouseHover;
+        public event MouseClickEventHandler MouseEnter;
+        public event MouseEnterEventHandler MouseLeave;
+        public event MouseLeaveEventHandler Click;
         #endregion
 
         public UIElement(Game game, Point location, Point size) : base(game)
@@ -34,6 +42,10 @@ namespace TerminalGame.UI.Elements
             _fadeTarget = 1;
             _fadingDown = false;
             _fadingUp = true;
+            _hover= new MouseEventArgs();
+            _click = new MouseEventArgs();
+            _enter = new MouseEventArgs();
+            _leave = new MouseEventArgs();
         }
 
         public override void Initialize()
@@ -56,6 +68,34 @@ namespace TerminalGame.UI.Elements
         {
             if (Enabled)
             {
+                _previousMouseState = _currentMouseState;
+                _currentMouseState = Mouse.GetState();
+
+                var mouseRectangle = new Rectangle(_currentMouseState.X, _currentMouseState.Y, 1, 1);
+
+                _isHovering = false;
+
+                if (mouseRectangle.Intersects(Rectangle))
+                {
+                    _isHovering = true;
+                    MouseHover?.Invoke(_hover);
+
+                    if (_currentMouseState.LeftButton == ButtonState.Released && _previousMouseState.LeftButton == ButtonState.Pressed)
+                    {
+                        Click?.Invoke(_click);
+                    }
+                }
+
+                _newMouseHoverState = _isHovering;
+                if (_newMouseHoverState != _previousMouseHoverState)
+                {
+                    if (_newMouseHoverState)
+                        MouseEnter?.Invoke(_enter);
+                    else
+                        MouseLeave?.Invoke(_leave);
+                }
+                _previousMouseHoverState = _newMouseHoverState;
+
                 if (_fadingUp)
                     FadeUp(_fadeTarget);
                 if (_fadingDown)
@@ -80,50 +120,47 @@ namespace TerminalGame.UI.Elements
             }
         }
 
-        protected virtual void OnMouseHover(object sender, EventArgs e)
-        {
-            MouseHover?.Invoke(this, e);
-        }
-
-        protected virtual void OnMouseEnter(object sender, EventArgs e)
-        {
-            MouseEnter?.Invoke(this, e);
-        }
-
-        protected virtual void OnMouseLeave(object sender, EventArgs e)
-        {
-            MouseLeave?.Invoke(this, e);
-        }
-
-        protected virtual void OnClick(object sender, EventArgs e)
-        {
-            Click?.Invoke(this, e);
-        }
-
+        /// <summary>
+        /// Fades the element out to 50% transparency
+        /// </summary>
         public void Dim()
         {
             _fadeTarget = 0.5f;
             _fadingDown = true;
         }
 
+        /// <summary>
+        /// Fades the element up to 100% opacity
+        /// </summary>
         public void UnDim()
         {
             _fadeTarget = 1.0f;
             _fadingUp = true;
         }
 
+        /// <summary>
+        /// Fades the element down to 100% transparency
+        /// </summary>
         public void FadeOut()
         {
             _fadeTarget = 0.0f;
             _fadingDown = true;
         }
 
+        /// <summary>
+        /// Fades the element up to 100% opacity 
+        /// </summary>
         public void FadeIn()
         {
             _fadeTarget = 1.0f;
             _fadingUp = true;
         }
 
+        /// <summary>
+        /// Fades the element up to <c>target</c> opacity at <c>delta</c> speed
+        /// </summary>
+        /// <param name="target">Target opacity</param>
+        /// <param name="delta">Fade speed</param>
         private void FadeUp(float target, float delta = 0.01f)
         {
             _opacity += delta;
@@ -141,6 +178,11 @@ namespace TerminalGame.UI.Elements
             }
         }
 
+        /// <summary>
+        /// Fades the element down to <c>target</c> opacity at <c>delta</c> speed
+        /// </summary>
+        /// <param name="target">Target opacity</param>
+        /// <param name="delta">Fade speed</param>
         private void FadeDown(float target, float delta = 0.01f)
         {
             _opacity -= delta;
@@ -201,5 +243,13 @@ namespace TerminalGame.UI.Elements
         {
             base.OnVisibleChanged(sender, args);
         }
+    }
+
+    /// <summary>
+    /// Generic mouse event args
+    /// </summary>
+    public class MouseEventArgs : EventArgs
+    {
+        // TODO: Specify MouseEventArg content
     }
 }
