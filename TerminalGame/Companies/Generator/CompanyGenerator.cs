fcebbf71e7d67ec5a792fe30fed87ca380aa1993
balderi <balderi@tsponline.dk>
2019-06-10 08:32:42 +0200

    added more variations in company names

--- TerminalGame/Companies/Generator/CompanyGenerator.cs
+++ TerminalGame/Companies/Generator/CompanyGenerator.cs
@@ -7,8 +7,8 @@ namespace TerminalGame.Companies.Generator
 {
     public static class CompanyGenerator
     {
-        private static readonly string[] aName = new string[] { "Advanced", "Air", "Aero", "Applied", "Auto", "Bank", "Best", "City", "Chem", "Com", "Commercial", "Compu", "Cyber", "Dyn", "Dyna", "Dyno", "East", "Eastern", "Energy", "Equity", "Excellent", "Excellency", "Express", "Gen", "General", "Green", "Health", "Home", "Ini", "Initial", "International", "Key", "Max", "Mac", "Med", "Medical", "Micro", "National", "North", "Northern", "Office", "Omni", "Pharma", "Poly", "Quantum", "Regional", "Safe", "Sci", "South", "Southern", "Super", "Synthetic", "Synth", "West", "Western" };
-        private static readonly string[] bName = new string[] { " Shipping", " Logistics", " Renovation", "Consult", " Consulting", " Construction", " Estates", " Power", " Electric", " Services", " Computer Services", " Technologies", " Tech", " Technology", " Energy", "field", "Field", "dyne", "Dyne", "tyne", "Tyne", " Energy", " Pharmaceuticals", " Pharma", "Pharm", " Financial", " Devices", " International", " Industial Technologies", "land", "vox", "Vox", "tel", "Tel", "Nation", "net", "Net", " Group", " Products", "Zone", " Homes", " Software", "Soft", "point", " Point", " Worldwide", "vision", "Vision", "Star", "ware", " Brands", "Air", " Air", " Airlines", " Airways", "Auto", " Auto", " Foods", " Healthcare" };
+        private static readonly string[] aName = new string[] { "Alpha", "Gamma", "Delta", "Epsilon", "Lambda", "Omicron", "Sigma", "Omega", "Holo", "Crypto", "Advanced", "Air", "Aero", "Applied", "Auto", "Bank", "Best", "City", "Chem", "Com", "Commercial", "Compu", "Cyber", "Dyn", "Dyna", "Dyno", "East", "Eastern", "Energy", "Equity", "Excellent", "Excellency", "Express", "Gen", "General", "Green", "Health", "Home", "Ini", "Initial", "International", "Key", "Max", "Mac", "Med", "Medical", "Micro", "National", "North", "Northern", "Office", "Omni", "Pharma", "Poly", "Quantum", "Regional", "Safe", "Sci", "South", "Southern", "Super", "Synthetic", "Synth", "West", "Western" };
+        private static readonly string[] bName = new string[] { "help", "Help", " Help", " Service", " Services", " Shipping", " Logistics", " Renovation", "Consult", " Consulting", " Construction", " Estates", " Power", " Electric", " Services", " Computer Services", " Technologies", " Tech", " Technology", " Energy", "field", "Field", "dyne", "Dyne", "tyne", "Tyne", " Energy", " Pharmaceuticals", " Pharma", "Pharm", " Financial", " Devices", " International", " Industial Technologies", "land", "vox", "Vox", "tel", "Tel", "Nation", "net", "Net", " Group", " Products", "Zone", " Homes", " Software", "Soft", "point", " Point", " Worldwide", "vision", "Vision", "Star", "ware", " Brands", "Air", " Air", " Airlines", " Airways", "Auto", " Auto", " Foods", " Healthcare" };
         private static readonly string[] suffix = new string[] { ", Inc.", " Corp.", " Corporation", " Incorporated", ", Ltd.", " Company", " Co.", " LLC", " Intl.", " International" };
 
         private static Random _rnd;
@@ -60,19 +60,16 @@ namespace TerminalGame.Companies.Generator
                 case 5:
                 case 7:
                     {
-                        //MainWIndowFM.ShowMessage("case 0");
                         return aName[_rnd.Next(0, aName.Length)] + bName[_rnd.Next(0, bName.Length)] + suffix[_rnd.Next(0, suffix.Length)];
                     };
                 case 1:
                     {
-                        //MainWIndowFM.ShowMessage("case 1");
                         return aName[_rnd.Next(0, aName.Length)] + bName[_rnd.Next(0, bName.Length)];
                     };
                 case 2:
                 case 4:
                 case 6:
                     {
-                        //MainWIndowFM.ShowMessage("case 2");
                         return aName[_rnd.Next(0, aName.Length)] + suffix[_rnd.Next(0, suffix.Length)];
                     };
                 default:
